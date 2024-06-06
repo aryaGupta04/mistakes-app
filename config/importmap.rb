3f82cb7e93beac8d10ae8750de71e42289125535
Arya Gupta <aryagupta@2005---Solutions-MacBook-Pro.local>
2024-06-06 14:14:18 -0400

    tito did this

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "jquery", preload: true
+pin "use_jquery", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
